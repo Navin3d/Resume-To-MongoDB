c9092e67af2a37087a65d1e7670d4e69cf70d9fb
Navin3d <smnavin65@gmail.com>
2022-12-28 00:27:50 +0530

    Code clean

--- src/services/pdf-service.js
+++ src/services/pdf-service.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-const pdf = require('pdf-parse');
 const PDFExtract = require('pdf.js-extract').PDFExtract;
 const pdfExtract = new PDFExtract();
 
@@ -11,22 +9,9 @@ const statusCheck = (req, res) => (
     })
 );
 
-const simplePdfUpload= (req, res) => {
-    upload(req, res, (err) => {
-        let dataBuffer = fs.readFileSync(req.files[0].path);  
-        pdf(dataBuffer).then(function(data) {
-            res.send({"jsondata":data,})
-        })
-        .catch(function(error){
-        })
-    })
-}
-
 const extractText = async (req, res) => {
-//     const buffer = fs.readFileSync(req.files[0].path);
-
     let fileData = req.files.files["data"];
-    const options = {}; /* see below */
+    const options = {};
     pdfExtract.extractBuffer(fileData, options, (err, data) => {
         if (err) return console.log(err);
         console.log(data);
